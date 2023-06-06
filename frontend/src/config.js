23961ce56b45363a8d9d7ef9a8254bcb5509df07
Arnab Banik <84622873+ArnabBanik-repo@users.noreply.github.com>
2023-06-06 18:34:11 +0530

    Update config.js
    Optional backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URI="https://social-media-api-uggj.onrender.com";
-export const BACKEND_URI="https://soicialnet.onrender.com";
+export const BACKEND_URI="https://social-media-api-uggj.onrender.com";
+// export const BACKEND_URI="https://soicialnet.onrender.com";
